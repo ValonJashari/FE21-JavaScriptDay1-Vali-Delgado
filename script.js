d463fa1fe3f895b34711b62026a4cb15ccc59eec
Freddy <freddy.delgado.porras@gmail.com>
2021-03-03 10:51:31 +0100

    we added basic exercise

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Basic Exercise 1 */
+
+var text = "Hello from the Console";
+console.log(text);
+
+var text_2 = "Hello from the Browser";
+document.write(text_2);
+
+/* Basic Exercise 2 */
+
+var text_3 = "Hi my name is Martin";
+console.log(text_3);
+
+/* Basic Exercise 3 */
+
+var age = 36;
+var name = "Freddy";
+
+/* Basic Exercise 4 */
+
+var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
+console.log(players[2]);
+
+/* Intermediate Exercise 1 */
