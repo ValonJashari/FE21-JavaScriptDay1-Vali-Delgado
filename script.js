bb24dbe8255776361f4c094597f2d309f066cf6d
Freddy <freddy.delgado.porras@gmail.com>
2021-03-03 12:01:41 +0100

    we added excercise int.1-2

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log(text_3);
 
 /* Basic Exercise 3 */
 
-var age = 36;
+var age = 30;
 var name = "Freddy";
 
 /* Basic Exercise 4 */
@@ -22,3 +22,22 @@ var players = ["Martin", "Thomas", "Peter", "Mathias", "Niki"];
 console.log(players[2]);
 
 /* Intermediate Exercise 1 */
+
+var autos = ["Tesla", "Audi", "Rnault", "Volvo", "Mazda", "Fiat", "Ferrari"];
+console.log(autos.sort());
+
+/* Intermediate Exercise 1a */
+
+var fruits = ["apple", "banana", "kiwi"];
+fruits.push("orange");
+
+var fruits2 = ["apple", "banana", "kiwi", "orange"];
+fruits2.pop("orange");
+console.log(fruits2);
+
+/* Intermediate Exercise 1b */
+
+var a = 'Monkey horse dog.';
+var animals = new Array();
+animals = a.split(' '); // dont forget the space
+console.log(animals);
